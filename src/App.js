19a3aeed92b7d655af92ad2e4de53e6926071b9a
jdmunozg <juan.david.m.g1004@gmai.com>
2021-11-23 21:25:26 -0500

    ll

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-//import React from "react";
 import Header from './components/cabecera/header';
 import Histori from './components/historias/histori'
 import Pincard from "./components/carta/pincard";
